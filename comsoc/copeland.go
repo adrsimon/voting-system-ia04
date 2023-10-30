2e7d35023f96c042919f5244928a14940ffc4d47
Adrien Simon <adrisim2001@gmail.com>
2023-10-30 19:34:39 +0100

    copeland

---
+++ comsoc/copeland.go
@@ -0,0 +1,24 @@
+package comsoc
+
+func CopelandSWF(p Profile) (count Count, err error) {
+	count = make(Count)
+	for _, v := range p {
+		for i := 0; i < len(v); i++ {
+			for j := i + 1; j < len(v); j++ {
+				if v[i] != v[j] {
+					if isPref(v[i], v[j], v) {
+						count[v[i]]++
+					} else {
+						count[v[j]]++
+					}
+				}
+			}
+		}
+	}
+	return count, nil
+}
+
+func CopelandSCF(p Profile, _ ...int64) (bestAlts []Alternative, err error) {
+	count, err := CopelandSWF(p)
+	return maxCount(count), err
+}
